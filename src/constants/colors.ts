b5d34a8d21023371cc02b24bbdde01756d5098ac
Maciek <maciejciepal@gmail.com>
2022-05-29 22:26:56 +0200

    Added constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+const COLORS = {
+  gradientOneTopColor: 'rgb(90,100,225)',
+  gradientOneBottomColor: 'rgb(105,90,225)',
+  backgroundColor: 'rgb(245,245,250)',
+  inputColor: 'rgb(255,255,255)',
+  textColor: 'rgb(35,35,70)',
+  headerTextColor: '#fff',
+}
+
+const GRADIENTS = {
+  gradientOne: ['rgb(90,100,225)', 'rgb(105,90,225)'],
+  gradientTwo: ['rgb(80, 90, 215)', 'rgb(115, 135, 225)'],
+  gradientThree: ['rgb(240, 165, 190)', 'rgb(230, 105, 145)'],
+  gradientFour: ['rgb(235,165,150)', 'rgb(130, 100, 140)'],
+  gradientFive: ['rgb(240, 205, 120)', 'rgb(240, 195, 80)'],
+}
+
+export { COLORS, GRADIENTS };
---
+++ src/constants/shadows.ts
@@ -0,0 +1,6 @@
+export const SHADOWS = {
+  shadowColor: '#000000',
+  shadowOffset: {width: 0, height: 0},
+  shadowOpacity: 0.1,
+  shadowRadius: 10,
+}
